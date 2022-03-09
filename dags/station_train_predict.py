bc7647f24ae95dc399f1adffd733a9734c568133
sfc-gh-mgregory <michael.gregory@snowflake.com>
2022-03-09 13:23:02 +0100

    started parallelization

--- dags/station_train_predict.py
+++ dags/station_train_predict.py
@@ -1,67 +1,51 @@
 
-def station_train_predict_func(historical_data:list, 
-                               historical_column_names:list, 
-                               target_column:str,
-                               cutpoint: int, 
-                               max_epochs: int, 
-                               forecast_data:list,
-                               forecast_column_names:list,
-                               lag_values:list) -> str:
-    
-    from torch import tensor
+def station_train_predict_func(input_data: list, 
+                               input_columns: list, 
+                               target_column: str,
+                               max_epochs: int) -> str:
+
     import pandas as pd
-    from pytorch_tabnet.tab_model import TabNetRegressor
-    from datetime import timedelta
-    import numpy as np
+    df = pd.DataFrame(input_data, columns = input_columns)
     
-    feature_columns = historical_column_names.copy()
-    feature_columns.remove('DATE')
-    feature_columns.remove('STATION_ID')
-    feature_columns.remove(target_column)
-    
-    df = pd.DataFrame(historical_data, columns = historical_column_names)
-    #df['DATE']=pd.to_datetime(df['DATE']).dt.date
-    
-    y_valid = df[target_column][-cutpoint:].values.reshape(-1, 1)
-    X_valid = df[feature_columns][-cutpoint:].values
-    y_train = df[target_column][:-cutpoint].values.reshape(-1, 1)
-    X_train = df[feature_columns][:-cutpoint].values
-    
-    batch_df = pd.DataFrame(range(2,65,2), columns=['batch_size'])
-    batch_df['batch_remainder'] = len(X_train)%batch_df['batch_size']
-    optimal_batch_size=int(batch_df['batch_size'].where(batch_df['batch_remainder']==batch_df['batch_remainder'].min()).max())
-    
-    model = TabNetRegressor()
+    #Due to annual seasonality we need at least one year of data for training 
+    #and a second year of data for validation
+    if len(df) < 365*2:
+        df['PRED'] = 'NULL'
+    else:
+        feature_columns = input_columns.copy()
+        feature_columns.remove('DATE')
+        feature_columns.remove(target_column)
 
-    model.fit(
-        X_train, y_train,
-        eval_set=[(X_valid, y_valid)],
-        max_epochs=max_epochs,
-        patience=100,
-        batch_size=optimal_batch_size, 
-        virtual_batch_size=optimal_batch_size/2,
-        num_workers=0,
-        drop_last=True)
-    
-    df['PRED'] = model.predict(tensor(df[feature_columns].values))
+        from torch import tensor
+        from pytorch_tabnet.tab_model import TabNetRegressor
 
-    #Now make the multi-step forecast
-    if len(lag_values) > 0:
-        forecast_df = pd.DataFrame(forecast_data, columns = forecast_column_names)
-        
-        for step in range(len(forecast_df)):
-            station_id = df['STATION_ID'][-1:].values[0]
-            future_date = (pd.to_datetime(df.iloc[-1]['DATE'])+timedelta(days=1)).strftime('%Y-%m-%d')
-            lags=[df.shift(lag-1).iloc[-1]['COUNT'] for lag in lag_values]
-            forecast=forecast_df.loc[forecast_df['DATE']==future_date]
-            forecast=forecast.drop(labels='DATE', axis=1).values.tolist()[0]
-            features=[*lags, *forecast]
-            pred=round(model.predict(np.array([features]))[0][0])
-            row=[future_date, station_id, pred, *features, pred]
-            df.loc[len(df)]=row
+        model = TabNetRegressor()
+
+        #cutpoint = round(len(df)*(train_valid_split/100))
+        cutpoint = 365
+
+        ##NOTE: in order to do train/valid split on time-based portion the input data must be sorted by date    
+        df['DATE'] = pd.to_datetime(df['DATE'])
+        df = df.sort_values(by='DATE', ascending=True)
+
+        y_valid = df[target_column][-cutpoint:].values.reshape(-1, 1)
+        X_valid = df[feature_columns][-cutpoint:].values
+        y_train = df[target_column][:-cutpoint].values.reshape(-1, 1)
+        X_train = df[feature_columns][:-cutpoint].values
+
+        model.fit(
+            X_train, y_train,
+            eval_set=[(X_valid, y_valid)],
+            max_epochs=max_epochs,
+            patience=100,
+            batch_size=1024, 
+            virtual_batch_size=128,
+            num_workers=0,
+            drop_last=False)
+
+        df['PRED'] = model.predict(tensor(df[feature_columns].round(2).values))
+        df['DATE'] = df['DATE'].dt.strftime('%Y-%m-%d')
+        df = pd.concat([df, pd.DataFrame(model.explain(df[feature_columns].values)[0], 
+                               columns = feature_columns).add_prefix('EXPL_').round(2)], axis=1)
     
-    explain_df = pd.DataFrame(model.explain(df[feature_columns].astype(float).values)[0], 
-                         columns = feature_columns).add_prefix('EXPL_').round(2)
-    df = pd.concat([df.set_index('DATE').reset_index(), explain_df], axis=1)
-   
     return [df.values.tolist(), df.columns.tolist()]
