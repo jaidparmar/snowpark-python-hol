ac4e42055e70842888cb2f23bc2014001902f696
sfc-gh-mgregory <michael.gregory@snowflake.com>
2022-03-13 16:02:35 +0100

    cleanedup pipeline

--- dags/snowpark_connection.py
+++ dags/snowpark_connection.py
@@ -1,11 +1,10 @@
 
 def snowpark_connect(state_file='./include/state.json'):
     import snowflake.snowpark as snp
-    import os, json, uuid
+    import json
     
     with open(state_file) as sdf:
         state_dict = json.load(sdf)    
-    state_dict["model_id"] = str(uuid.uuid1()).replace('-', '_')
     
     session=None
     session = snp.Session.builder.configs(state_dict["connection_parameters"]).create()
